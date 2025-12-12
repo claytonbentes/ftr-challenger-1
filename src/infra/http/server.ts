7792814bc3f83ee3d7d1edda324668c9297f126e
Clayton Bentes <claytonbentes@live.com>
2025-12-12 07:30:03 -0300

    config: fix configs

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,65 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from '@/env'
+import { increaseAccessCountUrlRoute } from './routes/count-urls'
+import { createUrlRoute } from './routes/create-url'
+import { deleteUrlRoute } from './routes/delete-url'
+import { exportUrlRoute } from './routes/export-url'
+import { getOriginalUrl } from './routes/get-original-url'
+import { getUrlsRoute } from './routes/get-urls'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  // Envia o erro p/ alguma ferramenta de observabilidade (Sentry/DataDog/Grafana/OTel)
+
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
+
 server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+server.register(createUrlRoute)
+server.register(getUrlsRoute)
+server.register(getOriginalUrl)
+server.register(increaseAccessCountUrlRoute)
+server.register(deleteUrlRoute)
+server.register(exportUrlRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
